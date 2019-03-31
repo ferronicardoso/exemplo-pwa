9c46eeb6bf1861b165c838c0c2ca166f1e0f8c46
Raphael Cardoso <ferronicardoso@outlook.com>
2019-03-31 04:47:03 -0300

    .

--- sw.js
+++ sw.js
@@ -1,48 +1,42 @@
-// Cache on install
-var staticCacheName = 'exemplo_pwa_2019_02_16_20_13'
-this.addEventListener("install", event => {
-    this.skipWaiting();
+var CACHE_NAME = 'static-v1';
 
-    event.waitUntil(
-        caches.open(staticCacheName)
-            .then(cache => {
-                return cache.addAll([
-                    'offline',
-                    'assets/icons/android-icon-144x144.png',
-                    'assets/icons/android-icon-192x192.png',
-                    'assets/icons/android-icon-256x256.png',
-                    'assets/icons/android-icon-36x36.png',
-                    'assets/icons/android-icon-48x48.png',
-                    'assets/icons/android-icon-72x72.png',
-                    'assets/icons/android-icon-96x96.png'
-                ]);
-            })
-    )
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll([
+        'offline',
+        'assets/icons/android-icon-512x512.png',
+        'assets/icons/android-icon-144x144.png',
+        'assets/icons/android-icon-192x192.png',
+        'assets/icons/android-icon-256x256.png',
+        'assets/icons/android-icon-36x36.png',
+        'assets/icons/android-icon-48x48.png',
+        'assets/icons/android-icon-72x72.png',
+        'assets/icons/android-icon-96x96.png'
+      ]);
+    })
+  )
 });
 
-// Clear cache on activate
-this.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames
-                    .filter(cacheName => (cacheName.startsWith('exemplo_pwa_')))
-                    .filter(cacheName => (cacheName !== staticCacheName))
-                    .map(cacheName => caches.delete(cacheName))
-            );
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys
+        .filter(function (key) {
+          return key.indexOf(CACHE_NAME) !== 0;
         })
-    );
+        .map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
 });
 
-// Serve from Cache
-this.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-            .catch(() => {
-                return caches.match('/offline');
-            })
-    )
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
