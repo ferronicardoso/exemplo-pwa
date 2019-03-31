a1501cec41380143175176e27bf9bc6d9bd89510
Raphael Cardoso <ferronicardoso@outlook.com>
2019-03-31 03:55:20 -0300

    .

---
+++ sw.js
@@ -0,0 +1,41 @@
+// Cache on install
+var staticCacheName = 'exemplo_pwa_2019_02_16_20_13'
+this.addEventListener("install", event => {
+    this.skipWaiting();
+
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then(cache => {
+                return cache.addAll([
+                    '/offline'
+                ]);
+            })
+    )
+});
+
+// Clear cache on activate
+this.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(cacheName => (cacheName.startsWith('exemplo_pwa_')))
+                    .filter(cacheName => (cacheName !== staticCacheName))
+                    .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
+});
+
+// Serve from Cache
+this.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+            .catch(() => {
+                return caches.match('/offline');
+            })
+    )
+});
