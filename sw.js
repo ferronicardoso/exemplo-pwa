e143dcced06f6d20ecbd72b0fe3610a0238121d1
Raphael Cardoso <ferronicardoso@outlook.com>
2019-04-01 08:43:26 -0300

    .

--- sw.js
+++ sw.js
@@ -4,6 +4,7 @@ self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll([
+        '/css/default.cs',
         'assets/icons/android-icon-512x512.png',
         'assets/icons/android-icon-144x144.png',
         'assets/icons/android-icon-192x192.png',
